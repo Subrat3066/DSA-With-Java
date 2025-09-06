c318a19cc49a02a492dec6d183d1f47962380c6d
{subrat3066} <{your_github_email}>
2025-09-06 10:56:56 +0530

    twopointers

---
+++ sliding window/constant_sw.java
@@ -0,0 +1,26 @@
+//Find the max sum between all possible subarrays of length k in nums
+
+public class constant_sw{
+    public static void main(String[] args)
+    {
+        int[] nums = {100,4,5,3,6,800,9};
+        System.out.print(constant(nums , 3));
+    }
+    public static int constant(int[]nums , int k)
+    {
+        int l = 0 , r = 0;
+        int sum = 0;
+
+        while(r < k)sum+=nums[r++];
+        int maxSum = sum;
+
+        while(r<nums.length-1)
+        {
+            sum -= nums[l++];
+            r++;
+            sum+= nums[r];
+            maxSum = Math.max(maxSum , sum);
+        }
+        return maxSum;
+    }
+}
