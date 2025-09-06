d30e4b754df07002e5438438b8d57b0367c7b077
{subrat3066} <{your_github_email}>
2025-09-06 13:07:36 +0530

    complete

--- sliding window/constant_sw.java
+++ sliding window/constant_sw.java
@@ -3,8 +3,8 @@
 public class constant_sw{
     public static void main(String[] args)
     {
-        int[] nums = {100,4,5,3,6,800,9};
-        System.out.print(constant(nums , 3));
+        int[] nums = {100,4,5,3000,6,80,9};
+        System.out.print(constantWindow(nums , 3));
     }
     public static int constant(int[]nums , int k)
     {
@@ -20,6 +20,24 @@ public class constant_sw{
             r++;
             sum+= nums[r];
             maxSum = Math.max(maxSum , sum);
+        }
+        return maxSum;
+    }
+    public static int constantWindow(int[]nums , int k)
+    {
+		int left = 0;
+		int right = 0;
+		int sum = 0;
+		int maxSum = Integer.MIN_VALUE;
+		
+		while(right<nums.length) 
+        {
+			sum = sum + nums[right++];
+			if(right-left == k) 
+            {
+				maxSum = Math.max(maxSum , sum);
+				sum = sum - nums[left++];
+            }
         }
         return maxSum;
     }
--- sliding window/variable_windowSize.java
+++ sliding window/variable_windowSize.java
@@ -1,6 +1,31 @@
+//Find the length of longest subarray which sum is smaller than or equal to the k
+
+
 public class variable_windowSize{
     public static void main(String[] args)
     {
-        
+        int[] nums = {2,5,1,10,10,7,2,1,4};
+        System.out.print(variableWindow(nums , 14));
+    }
+
+    public static int variableWindow(int[]nums , int k)
+    {
+        int l = 0 , r = 0;
+        int sum = 0 , maxLen = 0;
+
+        while(r < nums.length )
+        {
+            sum += nums[r];
+            if(sum > k)
+            {
+                sum -= nums[l++];
+            }
+            if(sum <= k)
+            {
+                maxLen = Math.max(maxLen , r-l+1);
+            }
+            r = r+1;
+        }
+        return maxLen;
     }
 }
