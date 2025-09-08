7641b6ec54647fbca352d0bc63263a6badd8d4c7
{subrat3066} <{your_github_email}>
2025-09-08 14:54:53 +0530

    1423.Maximum Sum you can Obtains

--- sliding window/questions/maximumPoints.java
+++ sliding window/questions/maximumPoints.java
@@ -3,7 +3,26 @@
 public class maximumPoints{
     public static void main(String[] args)
     {
-
+        int[] nums = {2,1,3,5,7,9};
+        System.out.print(solution(nums , 4));
     }
-    
+    public static int solution(int[]nums , int k)
+    {
+        int lSum = 0, maxSum = 0 , rIndex = nums.length-1;
+
+        for(int i = 0 ; i < k ; i++)
+        {
+            lSum += nums[i];
+        }
+        maxSum = lSum;
+
+        for(int i = k-1 ; i >= 0 ; i--)
+        {
+            lSum -= nums[i];
+            lSum += nums[rIndex--];
+            maxSum = Math.max(maxSum , lSum);
+        }
+        return maxSum;
+    }
+
 }
